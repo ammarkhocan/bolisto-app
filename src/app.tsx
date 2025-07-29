1a6eb879a8214397af333641a046b0e2a172fcc7
Ammar Ismail Khocan <ammarkhocan02@gmail.com>
2025-07-29 13:07:40 +0700

    nam variabel for card

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,17 @@ export function App() {
     <div>
       <h1>Bolisto App</h1>
       <button>Submit</button>
+      <Card />
+    </div>
+  );
+}
+
+export function Card() {
+  const name = "Ammar Ismail Khocan ";
+
+  return (
+    <div>
+      <h1>Nama saya : {name}</h1>
     </div>
   );
 }
